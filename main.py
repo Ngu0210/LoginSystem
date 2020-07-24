1cbff52efefd364f66f9a0847c3a8d175bc10b39
Ngu0210 <dannynguyen312@gmail.com>
2020-07-24 18:48:09 +1000

    Started Login System Project

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8d391fa09d0b74fb0dde2c7c072aebb0'
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html', title = 'Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form = form)
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
