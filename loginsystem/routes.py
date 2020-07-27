2c796fb54e1c28b70141f6f83334ba36ccd3bef7
Ngu0210 <dannynguyen312@gmail.com>
2020-07-27 18:12:43 +1000

    Added update profile picture function

--- loginsystem/routes.py
+++ loginsystem/routes.py
@@ -1,19 +1,36 @@
+import os, secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from loginsystem import app, db, bcrypt
 from loginsystem.forms import RegistrationForm, LoginForm, UpdateForm
 from loginsystem.models import User
 from flask_login import login_user, logout_user, current_user, login_required
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext 
+    picture_path = os.path.join(app.root_path, 'static\profile_pics', picture_fn)
+
+    output_size = (256, 256)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    
+    i.save(picture_path)
+
+    return picture_fn
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html', title = 'Home')
+    return render_template('index.html', title='Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
+    
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -26,9 +43,9 @@ def login():
             return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title = 'Login', form = form)
+    return render_template('login.html', title='Login', form=form)
 
-@app.route('/register', methods = ['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
@@ -40,20 +57,26 @@ def register():
         db.session.commit()
         flash('Your account has been created!', 'success')
         return redirect(url_for('login'))
-    return render_template('register.html', title = 'Register', form = form)
+    return render_template('register.html', title='Register', form=form)
 
-@app.route('/account', methods = ['GET', 'POST'])
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():  
     form = UpdateForm()
     if form.validate_on_submit():
+        if form.profile.data:
+            picture_file = save_picture(form.profile.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title = 'Account', image_file = image_file, form = form)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 @app.route('/logout')
 def logout():
