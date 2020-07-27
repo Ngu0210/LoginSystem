51eefcaa4ec7a9d252d82a392ee3da853d905cb4
Ngu0210 <dannynguyen312@gmail.com>
2020-07-28 00:19:33 +1000

    Update password is somehow is fixed now

--- loginsystem/routes.py
+++ loginsystem/routes.py
@@ -26,7 +26,7 @@ def send_reset_email(user):
     msg.body = f''' To reset your password, visit the following link: 
 {url_for('reset_token', token=token, _external=True)}
 
-If you did not make this request, simply ignore this email and no changes will be made.
+If you did not make this request, kill me now ignore this email and no changes will be made.
 '''
     mail.send(msg)
 
